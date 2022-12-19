052358d2980c0f16dd38e5561bdc3e531f99cb6f
jhjo-tech <jhjo.tech@gmail.com>
2022-12-19 20:50:48 +0900

    Add SwfitUI-Basic proj

---
+++ SwfitUI-Basic/SwfitUI-Basic/SwfitUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUI_BasicApp.swift
+//  SwfitUI-Basic
+//
+//  Created by Jo JANGHUI on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
