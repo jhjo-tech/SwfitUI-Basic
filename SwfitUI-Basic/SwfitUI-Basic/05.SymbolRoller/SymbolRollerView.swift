a4b98650fac343581bd40041625831a1c4c02dca
jhjo-tech <jhjo.tech@gmail.com>
2022-12-21 20:32:23 +0900

    Add SwiftUI 응용 - SymbolRoller

--- SwfitUI-Basic/SwfitUI-Basic/05.SymbolRoller/SymbolRollerView.swift
+++ SwfitUI-Basic/SwfitUI-Basic/05.SymbolRoller/SymbolRollerView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct SymbolRollerView: View {
     let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    
+    @State var imageName: String = "moon"
+    
     var body: some View {
+        VStack {
+            Spacer()
+            
+            Image(systemName: imageName)
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            
+            Spacer()
+            
+            Text(imageName)
+                .font(.system(size: 40, weight: .bold))
+            
+            Button {
+                print("Button Tapped")
+                
+                imageName = symbols.randomElement()!
+            } label: {
+                HStack {
+                    Image(systemName: "arrow.3.trianglepath")
+//                        .foregroundColor(.white)
+                    
+                    VStack {
+                        Text("Reload")
+                            .font(.system(size: 30,
+                                          weight: .bold,
+                                          design: .default))
+//                            .foregroundColor(.white)
+                        Text("click me to reload")
+//                            .foregroundColor(.white)
+                    }
+                }
+            }
+            .foregroundColor(.white) // 공통으로 tint color
+            .frame(maxWidth: .infinity,
+                   minHeight: 80)
+            .background(.pink)
+//            .clipShape(Capsule())
+            .cornerRadius(40)
+//            .padding()
+        }
+        
         Text("Hello Symbol Roller")
     }
 }
